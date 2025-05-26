ce5ba9b4417650edd4669f01b71cb00d74018476
tomh500 <tomh500@163.com>
2025-05-27 02:56:34 +0800

    build bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-g++ ./src/scfg.cpp ./src/lcfg_extra/LCFGFlagDevices.cpp -I./src/include -L./src/ -llua54 -o lcfg.exe -std=c++20 -static
+g++ ./src/scfg.cpp ./src/lcfg_extra/LCFGFlagDevices.cpp -I./src/include -L./src/ -llua54 -o scfg.exe -std=c++20 -static
 pause
